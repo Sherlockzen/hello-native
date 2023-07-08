8a5843bf2ff305d77fa31588582faf7dab520260
Hewerton <hewertonlourenco@gmail.com>
2023-07-08 01:05:14 -0300

    Feat: add image e.g

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text style={styles.pinkBackground}>
-        Aqui temos o exemplo de um texto simples!
-      </Text>
+      <Image source={require("./assets/realidade.png")} resizeMode="center" />
       <StatusBar style="auto" />
     </View>
   );
