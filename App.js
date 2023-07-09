6130f1365bf00ffd61f236a1c316633468c632a4
Hewerton <hewertonlourenco@gmail.com>
2023-07-09 10:42:53 -0300

    Feat: adding theme for the choose photo button

--- App.js
+++ App.js
@@ -2,17 +2,30 @@ import { StatusBar } from "expo-status-bar";
 import { Image, StyleSheet, Text, View } from "react-native";
 import ImageViewer from "./components/ImageViewer";
 import Button from "./components/Button";
+import * as ImagePicker from "expo-image-picker";
 
 const PlaceHolderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceHolderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label={"Choose a photo"} />
+        <Button theme={"primary"} label={"Choose a photo"} />
         <Button label={"Use this photo"} />
       </View>
       <StatusBar style="auto" />
