0f5f84fc92acb1fb7bb23c2f85c4543ca185545d
Hewerton <hewertonlourenco@gmail.com>
2023-07-11 11:20:15 -0300

    Feat: adding buttons to reset, add sticker and save

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import ImageViewer from "./components/ImageViewer";
 import Button from "./components/Button";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
+import IconButton from "./components/IconButton";
+import CircleButton from "./components/CircleButton";
 
 const PlaceHolderImage = require("./assets/images/background-image.png");
 
@@ -24,6 +26,17 @@ export default function App() {
       alert("You did not select any image.");
     }
   };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+  const onAddSticker = () => {
+    //Put code here
+  };
+  const onSaveImageAsync = () => {
+    //Put code here
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
@@ -33,7 +46,17 @@ export default function App() {
         />
       </View>
       {showAppOptions ? (
-        <View />
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon={"refresh"} label={"Reset"} onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon={"save-alt"}
+              label={"Save"}
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
       ) : (
         <View style={styles.footerContainer}>
           <Button
@@ -65,5 +88,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
